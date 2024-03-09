fe86ef2847d344df9aeaa81344c3101833fa13a5
Apoorva <apkpashu@gmail.com>
2024-03-09 22:28:12 +0530

    one more

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const ButtonList = () => {
 
-  const list = ['All','Cooking','News','Javascript','90s songs','Trekking','Sufi','Taylor Swift','Pink Floyd'];
+  const list = ['All','Cooking','News','Javascript','90s songs','Trekking','Sufi','Taylor Swift','Pink Floyd','Sky'];
   return (
     <div className="flex">
       {list.map(btn =>(
