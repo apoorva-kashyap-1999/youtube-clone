c616787fac9aac74626637ec1df5dc65845fedc4
Apoorva <apkpashu@gmail.com>
2024-03-09 22:27:19 +0530

    Buttons

---
+++ src/components/ButtonList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button";
+
+const ButtonList = () => {
+
+  const list = ['All','Cooking','News','Javascript','90s songs','Trekking','Sufi','Taylor Swift','Pink Floyd'];
+  return (
+    <div className="flex">
+      {list.map(btn =>(
+        <Button name={btn}/>))}
+    </div>
+  );
+};
+
+export default ButtonList;
