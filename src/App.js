3a7cf2a84aed0c1795b9e7d473e635f040f20420
Apoorva <apkpashu@gmail.com>
2024-03-09 19:54:32 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Head from './components/Head';
+// import Body from './components/Body';
 
 function App() {
   return (
     <div className="App">
-    <h1 className='text-xl'>Namaste React</h1>
+    <Head/>
+    {/* <Body/> */}
+    {/* 
+    Head
+    Body-
+      Sidebar-
+       Menu      
+      Main container-
+       Buttons List
+       Video Container-
+        Video Card
+     */}
     </div>
   );
 }
