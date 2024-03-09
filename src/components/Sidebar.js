f9d9f0d4cfd654bbcb22c702bf54bae05a816286
Apoorva <apkpashu@gmail.com>
2024-03-09 22:04:54 +0530

    emojis , window+;

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,24 +11,24 @@ const Sidebar = () => {
   return (
     <div className="p-5 shadow-lg w-48">
       <ul>
-        <li>Home</li>
-        <li>Shorts</li>
-        <li>Live</li>
-        <li>Videos</li>
+        <li>🏠Home</li>
+        <li>🎥Shorts</li>
+        <li>🔴Live</li>
+        <li>📽️Videos</li>
       </ul>
       <h1 className="font-bold pt-5">Subscriptions</h1>
       <ul>
-        <li>Music</li>
-        <li>Lifestyle</li>
-        <li>Fashion</li>
-        <li>Travel</li>
+        <li>🎶Music</li>
+        <li>🧬Lifestyle</li>
+        <li>👚Fashion</li>
+        <li>✈️Travel</li>
       </ul>
       <h1 className="font-bold pt-5">Watch Later</h1>
       <ul>
-        <li>Sports</li>
-        <li>Game</li>
-        <li>React</li>
-        <li>Spiritual</li>
+        <li>🎾Sports</li>
+        <li>🎮Game</li>
+        <li>📚React</li>
+        <li>🙏Spiritual</li>
       </ul>
     </div>
   );
