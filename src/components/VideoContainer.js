a1591cd1789d700b1a336b291e37781bace04702
Apoorva <apkpashu@gmail.com>
2024-04-14 22:58:48 +0530

    hoc

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import {YOUTUBE_VIDEOS_API} from '../util/constants'
 import VideoCard from './VideoCard';
+import {AdVideoCard}  from './VideoCard';
 import { Link } from 'react-router-dom';
 
 const VideoContainer = () => {
@@ -17,9 +18,11 @@ const VideoContainer = () => {
     console.log(json);
     setVideos(json.items);
   }
+  
   if(videos==null) return null;
   return (
     <div className='flex flex-wrap'>
+      <AdVideoCard key='ad' info={videos[0]}/>
     {videos.map((video)=><Link to={"/watch?v="+video.id} ><VideoCard key={video.id} info={video}/></Link>)}
     </div>
   )
