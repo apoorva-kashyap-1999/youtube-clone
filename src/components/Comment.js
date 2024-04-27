67e376f69baec44f2052fa86001fb804bb6bd8a2
Apoorva <apkpashu@gmail.com>
2024-04-27 20:29:11 +0530

    see replies

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import ReplyComments from "./ReplyComments";
 
 const Comment = ({ comments }) => {
+  const [showComment, setShowComment] = useState(false);
+  const toggleReplies = () => {
+    setShowComment(!showComment);
+  };
   return (
-    <ul>
-      {comments.map((comment, index) => (
-        <li
-          key={index}
-          className="p-2 m-2 border border-gray-100 bg-gray-200  shadow-lg rounded-lg text-left"
-        >
-          <div className="flex">
-            <img
-              className="h-12 inline"
-              alt="user"
-              src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
-            />
-            <div className="px-2 mx-2">
-              <span className="text-lg font-bold">{" " + comment.name}</span>
-              <div>{comment.text}</div>
+    <div>
+      <ul>
+        {comments.map((comment, index) => (
+          <li
+            key={index}
+            className="p-2 m-2 border border-gray-100 bg-gray-200  shadow-lg rounded-lg text-left"
+          >
+            <div className="flex">
+              <img
+                className="h-12 inline"
+                alt="user"
+                src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+              />
+              <div className="px-2 mx-2">
+                <span className="text-lg font-bold">{" " + comment.name}</span>
+                <div>{comment.text}</div>
+              </div>
             </div>
-          </div>
-          <div>
-            {comment.replies && (
-              <ReplyComments commentsReply={comment.replies} />
+            <div className="px-8 py-2 my-2 mx-8">
+              👍👎
+              <button className="mx-2" onClick={() => toggleReplies()}>🔽 See Replies</button>
+            </div>
+            {showComment && (
+              <div>
+                {comment.replies && (
+                  <ReplyComments commentsReply={comment.replies} />
+                )}
+              </div>
             )}
-            </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
