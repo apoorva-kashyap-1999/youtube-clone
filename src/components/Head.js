27d5cccf6e799d8b49e3121fe88f3ca6f6967c2f
Apoorva <apkpashu@gmail.com>
2024-04-27 13:07:28 +0530

    show suggestions

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../util/appSlice";
-import {YOUTUBE_SEARCH_API} from "../util/constants";
+import { YOUTUBE_SEARCH_API } from "../util/constants";
 
 const Head = () => {
   const dispatch = useDispatch();
@@ -9,24 +9,29 @@ const Head = () => {
     dispatch(toggleMenu());
   };
 
-  const [searchQuery, setSearchQuery]= useState("");
-  
-  const getSearchSuggestions = async function(){
-    const data = await fetch(YOUTUBE_SEARCH_API+searchQuery);
-    const json= await data.json();
-  }
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-  useEffect(()=>{
-   // console.log(searchQuery);
-   // Debouncing
-   // an api call after every key press if only diff between two inputs<200 , decline else make call
-    const timer = setTimeout(()=>getSearchSuggestions(), 2000); 
- 
+  const getSearchSuggestions = async function () {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+  };
+
+  useEffect(() => {
+    // console.log(searchQuery);
+    // Debouncing
+    // an api call after every key press if only diff between two inputs<200 , decline else make call
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+
     // need to make timer, when component re-renders
-    return(()=>{ clearTimeout(timer); })
+    return () => {
+      clearTimeout(timer);
+    };
 
     //dependency is searchquery , everytime it changes make a new api call
-  },[searchQuery]);
+  }, [searchQuery]);
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
@@ -38,23 +43,35 @@ const Head = () => {
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
         />
         <a href="/">
-        <img
-          className="h-8 mx-2"
-          alt="youtube-logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
-        /></a>
+          <img
+            className="h-8 mx-2"
+            alt="youtube-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+          />
+        </a>
       </div>
-
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-          value={searchQuery}
-          onChange={(e)=>setSearchQuery(e.target.value)}
-        />
-        <button className="border border-gray-400 bg-gray-100 py-2 px-4 rounded-r-full">
-          🔍
-        </button>
+        <div className="p-2">
+          <input
+            className="w-1/2 border border-gray-400 py-2 px-5 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={()=>setShowSuggestions(true)}
+            onBlur={()=>setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 bg-gray-100 py-2 px-5 rounded-r-full">
+            🔍
+          </button>
+        </div>
+
+        {showSuggestions && <div className="fixed bg-white mx-64 w-[34%] shadow-lg rounded-lg border border-gray-100">
+          <ul>
+            {suggestions.map(suggestion=>
+              <li className="py-2 hover:bg-gray-100 rounded-lg text-left p-3 ">🔍 {suggestion}</li>
+            )}
+          </ul>
+        </div>}
       </div>
 
       <div className="col-span-1">
